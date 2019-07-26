b8395fff1083a93504c930c2df9e881f668f8dec
4423 <hanamiya4423@gmail.com>
2019-07-26 13:52:19 +0900

    port

--- index.js
+++ index.js
@@ -2,4 +2,4 @@ const http = require('http');
 http.createServer(function(req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end("Hello Azure!");
-}).listen(port);
+}).listen(process.env.PORT);
