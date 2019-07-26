eefa79ac5c456844bdca5d93c302ee7670ca4d02
4423 <hanamiya4423@gmail.com>
2019-07-26 13:19:04 +0900

    80

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 var http = require('http');
-var fs = require('fs');
-var server = http.createServer(function(req, res) {
-  fs.readFile('index.html','UTF-8', (error, html) => {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write(html);
-    res.end();
-  });
-}).listen(8080);
+http.createServer(function(req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.write("Hello world!");
+  res.end();
+}).listen(80);
